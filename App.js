7b968140864da41172c8e38e4dd27336c7d803ef
Henry-Barreto <Henrique040701@gmail.com>
2026-05-16 16:04:24 -0300

    Refactor App component structure and remove unused code

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import estilos from './src/styleSheet/estilos';
+import Cabecalho from './src/componentes/Cabecalho';
+import Conteudo from './src/componentes/Conteudo';
+import Rodape from './src/componentes/Rodape';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilos.container}>
+      <Cabecalho />
+      <Conteudo />
+      <Rodape />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
